d5819ec6f000f62398da48da612f09d79c380472
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-05-05 09:59:14 +0530

    Create Lab 3- Q1.java

---
+++ LAB 1/Lab 3- Q1.java
@@ -0,0 +1,14 @@
+/* NAME : SURAVI DAS */
+public class lab3_1 {
+    public static void main(String[] args) {
+       
+  
+        System.out.println("NAME: SURAVI DAS");
+        System.out.println("ROLL: 20051388");
+        System.out.println("SECTION: CSE-18");
+        System.out.println("BRANCH: CSE");
+
+
+    }
+
+}
