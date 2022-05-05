daa94f23ffed09498c7c5058ef4edf44e5e359bd
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-05-05 11:26:31 +0530

    Create Lab 5-Q1.java

---
+++ LAB 3/Lab 5-Q1.java
@@ -0,0 +1,21 @@
+class a
+{
+    a(int i)
+    {
+        System.out.println(i);
+    }
+}
+class b extends a
+{
+    b()
+   {
+        super(20);
+    }
+}
+public class lab5_6 
+{
+    public static void main(String[] args) 
+    {
+        b obj=new b();
+    }
+}
