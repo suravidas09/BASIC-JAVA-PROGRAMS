5980037e5860d4c61f006d124eb0f48b14aecbb3
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-05-12 21:25:05 +0530

    Create Lab 4- Q5.java

---
+++ LAB 4/Lab 4- Q5.java
@@ -0,0 +1,24 @@
+public class lab4_5
+{
+	static int c=0;
+
+	public static void main(String[] args)
+	{
+		lab4_5 ob1=new lab4_5();
+		lab4_5 ob2=new lab4_5();
+		lab4_5 ob3=new lab4_5();
+		lab4_5 ob4=new lab4_5();
+		
+		ob1.finalize();
+		ob2.finalize();
+		ob3.finalize();
+		ob4.finalize();
+        
+		System.out.println("No. of objects destroyed is "+c);
+   }
+
+   protected void finalize()
+   {
+	   c++;
+   }
+}
