2c3ee93bc31295035181af79f7706247d71e4373
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-05-12 21:15:42 +0530

    Create Lab 4- Q1.java

---
+++ LAB 4/Lab 4- Q1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class lab4_1 
+{
+    public static void main(String[] args) 
+    {
+        int x, y, z;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        x = scan.nextInt();
+        System.out.print("Enter the second number:");
+        y = scan.nextInt();
+        System.out.print("Enter the third number:");
+        z = scan.nextInt();
+        scan.close();
+        if(x > y && x > z)
+        {
+            System.out.println("Largest number is:"+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("Largest number is:"+y);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+z);
+        }
+ 
+    }
+}
