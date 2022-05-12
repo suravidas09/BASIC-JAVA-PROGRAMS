d596ed20c4d025ff3bcca9747345b879f5612170
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-05-12 23:33:40 +0530

    Create Lab 5- Q3.java

---
+++ LAB 5/Lab 5- Q3.java
@@ -0,0 +1,37 @@
+class a
+{
+    void callme()
+    {
+        System.out.println("Inside a ");
+    }
+}
+class b extends a
+{
+    void callme()
+    {
+        System.out.println("Inside b ");
+    }
+}
+class c extends b
+{
+    void callme()
+    {
+        System.out.println("Inside c ");
+    }
+}
+public class lab5_3 
+{
+    public static void main(String[] args) 
+    {
+        a A= new a();
+        b B= new b();
+        c C= new c();
+        a r;
+        r=A;
+        r.callme();
+        r=B;
+        r.callme();
+        r=C;
+        r.callme();
+    } 
+}
