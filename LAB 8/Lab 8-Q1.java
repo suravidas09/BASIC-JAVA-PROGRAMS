ec67f7224c704df1af3a22a4c0acc8d0093be109
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-06-03 18:55:05 +0530

    Create Lab 8-Q1.java

---
+++ LAB 8/Lab 8-Q1.java
@@ -0,0 +1,15 @@
+public class lab8_1 
+{
+    public static void main(String[ ] args) 
+    {
+      try 
+      {
+        int[] myNumbers = {1, 2, 3};
+        System.out.println(myNumbers[10]);
+      } 
+      catch (Exception e) 
+      {
+        System.out.println("Something went wrong.");
+      }
+    }
+  }
