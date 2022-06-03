82ee3e92868df4f04a3b8996d1344e70f44f0431
suravidas09 <82047989+suravidas09@users.noreply.github.com>
2022-06-03 19:18:02 +0530

    Update and rename Lab 5- Q1.java to Lab 3- Q1.java

---
+++ LAB 3/Lab 3- Q1.java
@@ -0,0 +1,13 @@
+public class lab3_1 {
+    public static void main(String[] args) {
+       
+  
+        System.out.println("NAME: SURAVI DAS");
+        System.out.println("ROLL: 20051388");
+        System.out.println("SECTION: CSE-18");
+        System.out.println("BRANCH: CSE");
+
+
+    }
+
+}
